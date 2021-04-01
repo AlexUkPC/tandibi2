2f52c6d147f4334a30e668229f659c7753064a3c
AlexUkPC <alexrogna@yahoo.com>
2021-04-01 13:23:05 +0100

    changed to WSL

--- app/decorators/sight_decorator.rb
+++
@@ -1,8 +0,0 @@
-module SightDecorator
-  def humanized_activity_type
-    case activity_type
-    when Sight::CHECKIN then "checked in at"
-    when Sight::CHECKOUT then "checked out from"
-    end
-  end
-end
